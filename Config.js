362375848abd8e9290015088bbc022f99b94f2d2
mohdashiq18 <moashiq2018@gmail.com>
2023-10-16 11:31:23 +0530

    new

--- Config.js
+++ Config.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose")
-const Connect = mongoose.connect("mongodb+srv://ashiq:Rashiq@cluster0.bqvhbdc.mongodb.net/blogdata?retryWrites=true&w=majority")
+const Connect = mongoose.connect("mongodb+srv://triserv360test:triserv360@cluster0.lra4cam.mongodb.net/nitindhawan?retryWrites=true&w=majority")
 
 module.exports={
-    Connect
+    Connect 
 }  
+ 
